2d97e3f8c0c27f5faae321d6f46a121b3801f03b
Daniel Khapun <daniel.khapun@gmail.com>
2023-05-18 14:56:15 +0300

    added bulk startup scripts

--- run_icm_pretrainig.sh
+++ run_icm_pretrainig.sh
@@ -6,9 +6,9 @@ conda activate urlb2
 if [ $1 = "pretrain" ]; then
 	python pretrain.py group_name=$2 agent=$3 domain=$4 seed=$5 obs_type=$6
 elif [ $1 = "pre" ]; then
-	python pretrain.py $@
+	python pretrain.py ${@:2}
 elif [ $1 = "fine" ]; then
-	python finetune.py $@
+	python finetune.py ${@:2}
 elif [ $1 = "finetune" ]; then
 	python finetune.py group_name=$2 pretrained_agent=$3 task=$4 snapshot_ts=$5  seed=$6 obs_type=$7
 elif [ $1 = "finetuned" ]; then
--- start_all.sh
+++ start_all.sh
@@ -1,2 +1,2 @@
-sbatch --export=ALL,A="pre group_name=pretrain_icm_walker_pix2 pretrained_agent=icm domain=walker seed=16 obs_type=pixels buffer_dir=/code/url_benchmark/icm_walker_pixel_buffer2" ./urlb_job_git.sh
-sbatch --export=ALL,A="pre group_name=pretrain_icm_quad_pix2 pretrained_agent=icm domain=quadrupted seed=16 obs_type=pixels buffer_dir=/code/url_benchmark/icm_quad_pixel_buffer4" ./urlb_job_git.sh
+sbatch --export=ALL,A="pre group_name=pretrain_${1}_walker_pix2 agent=${1} domain=walker seed=16 obs_type=pixels buffer_dir=/code/url_benchmark/${1}_walker_pixel_buffer2" ./urlb_job_git.sh
+sbatch --export=ALL,A="pre group_name=pretrain_${1}_quad_pix4 agent=${1} domain=quadrupted seed=16 obs_type=pixels buffer_dir=/code/url_benchmark/${1}_quad_pixel_buffer4" ./urlb_job_git.sh
