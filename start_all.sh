4b0494b3dd9d582f8638ba6f79df780ce8aa6198
Daniel Khapun <daniel.khapun@gmail.com>
2023-06-15 15:54:30 +0300

    fix config

--- start_all.sh
+++ start_all.sh
@@ -1,4 +1,4 @@
-sbatch --export=ALL,A="pre group_name=pretrain_${1}_walker_pix_fix agent=${1} domain=walker seed=16 obs_type=pixels action_repeat=2 buffer_dir=/code/url_benchmark/${1}_walker_pixel_buffer4" ./urlb_job_git.sh
-sbatch --export=ALL,A="pre group_name=pretrain_${1}_quad_pix_fix agent=${1} domain=quadruped seed=16 obs_type=pixels action_repeat=2 buffer_dir=/code/url_benchmark/${1}_quad_pixel_buffer5" ./urlb_job_git.sh
-sbatch --export=ALL,A="pre group_name=pretrain_${1}_walker_pix_fix agent=${1} domain=walker seed=26 obs_type=pixels action_repeat=2 buffer_dir=/code/url_benchmark/${1}_walker_pixel_buffer5" ./urlb_job_git.sh
-sbatch --export=ALL,A="pre group_name=pretrain_${1}_quad_pix_fix agent=${1} domain=quadruped seed=26 obs_type=pixels action_repeat=2 buffer_dir=/code/url_benchmark/${1}_quad_pixel_buffer6" ./urlb_job_git.sh
+sbatch --export=ALL,A="pre group_name=pretrain_${1}_walker_${2} agent=${1} domain=walker seed=16 obs_type_params=${2} buffer_dir=/code/url_benchmark/${1}_walker_${2}_buffer4" ./urlb_job_git.sh
+sbatch --export=ALL,A="pre group_name=pretrain_${1}_quadruped_${2} agent=${1} domain=quadruped seed=16 obs_type_params=${2} buffer_dir=/code/url_benchmark/${1}_quad_${2}_buffer5" ./urlb_job_git.sh
+sbatch --export=ALL,A="pre group_name=pretrain_${1}_walker_${2} agent=${1} domain=walker seed=26 obs_type_params=${2} buffer_dir=/code/url_benchmark/${1}_walker_${2}_buffer5" ./urlb_job_git.sh
+sbatch --export=ALL,A="pre group_name=pretrain_${1}_quadruped_${2} agent=${1} domain=quadruped seed=26 obs_type_params=${2} buffer_dir=/code/url_benchmark/${1}_quad_${2}_buffer6" ./urlb_job_git.sh
