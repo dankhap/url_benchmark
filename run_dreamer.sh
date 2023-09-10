68bf147371efc3013b4ee46b4bbca24b44c0ea00
Daniel Khapun <daniel.khapun@gmail.com>
2023-09-10 15:42:02 +0300

    added pretrain sh script

--- run_dreamer.sh
+++ run_dreamer.sh
@@ -6,8 +6,8 @@ eval "$(conda shell.bash hook)"
 conda activate urlb2
 
 if [ $1 = "fine" ]; then
-	python finetune.py group_name=tests frame_stack=1 eval_every_frames= use_wandb=false pretrained_agent=icm task=walker_walk snapshot_ts=0 obs_type=pixels seed=8 num_seed_frames=2000 replay_buffer_num_workers=0 buffer_dir=/code/url_benchmark/icm_walker_pixels_buffer_bak/ load_only_encoder=true dreamer_conf.dreamer.pretrain=$1 dreamer_conf.dreamer.offline_skip_reward=$2
-else 
 	python finetune.py ${@:2}
+else 
+	python pretrain_dreamer.py ${@:2}
 fi
 
