5b9f2af9ba3912245ff5c23bab2e8089fd1fd8cf
Daniel Khapun <daniel.khapun@gmail.com>
2024-02-01 11:24:15 +0200

    fix run script

--- run_icm_pretrainig.sh
+++ run_icm_pretrainig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "in bash script"
-export PATH=$PATH:/opt/conda/envs/urlb2/bin:/opt/conda/condabin
+export PATH=$PATH:/opt/conda/envs/urlb2/bin:/opt/conda/bin
 echo "all arguments: $@"
 eval "$(conda shell.bash hook)"
 conda activate urlb2
